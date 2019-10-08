102ab4de4fe16c10505366f063cfcd5a46d31c61
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-10-08 16:37:27 -0700

    Steps 4-5 -- title and dated showing but no click down

--- Article/Article.js
+++ Article/Article.js
@@ -87,37 +87,59 @@ const data = [
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
   }
 ];
+const container = document.querySelector('.articles');
 
-function articleCreator(){
+data.map(cb => {
+  container.appendChild(articleCreator(
+    cb.title, cb.date, cb.firstParagraph, cb.secondParagraph, cb.thirdParagraph
+  ))
+});
+
+function articleCreator(title, date, firstParagraph, secondParagraph, thirdParagraph){
 
   // --- .createElement
   const article = document.createElement('div');
-  const title = document.createElement('h2');
-  const date = document.createElement('p')
-  const paragraph1 = document.createElement('p');
-  const paragraph2 = document.createElement('p');
-  const paragraph3 = document.createElement('p');
-  const button = document.createElement('button');
+  const articleTitle = document.createElement('h2');
+  const articleDate = document.createElement('p')
+  const articleParagraph1 = document.createElement('p');
+  const articleParagraph2 = document.createElement('p');
+  const articleParagraph3 = document.createElement('p');
+  const articleButton = document.createElement('span');
 
   // --- .classList --
   article.classList.add('article');
-  date.classList.add('date')
-  button.classList.add('expandButton')
+  articleDate.classList.add('date')
+  articleButton.classList.add('expandButton')
 
   // --- .textContent -- 
-  title.textContent = title;
-  date.textContent = date;
-  paragraph1.textContent = firstParagraph;
-  paragraph2.textContent = secondParagraph;
-  paragraph3.textContent = thirdParagraph;
+  articleTitle.textContent = title;
+  articleDate.textContent = date;
+  articleParagraph1.textContent = firstParagraph;
+  articleParagraph2.textContent = secondParagraph;
+  articleParagraph3.textContent = thirdParagraph;
 
   // --- .addEventListener
-  button.addEventListener('click', (e) =>{
+  articleButton.addEventListener('click', (e) =>{
     article.classList.toggle('article-open');
   })
 
+  // --- .appendChild -- 
+  article.appendChild(articleTitle);
+  article.appendChild(articleDate);
+  article.appendChild(articleParagraph1);
+  article.appendChild(articleParagraph2);
+  article.appendChild(articleParagraph3);
+  article.appendChild(articleButton);
+
   return article;
 }
+
+container.appendChild(articleCreator(
+  'Heyo, River!', 'Sep 29 2019', '"Four score and seven years ago our fathers brought forth on this continent',
+  ', a new nation, conceived in Liberty,', 'and dedicated to the proposition that all men are created equal.'
+))
+
+
 
 
 /* Step 1: Create a function that creates a component. You will want your component to look like the template below: 
